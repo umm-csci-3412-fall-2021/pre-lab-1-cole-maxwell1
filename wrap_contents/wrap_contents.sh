750ec02b474d5e1b8601d4cc7028ab83e518ccc1
cole-maxwell1 <cole.n.maxwell99@gmail.com>
2021-09-08 17:20:50 -0500

    Escape headerFooterName variable

--- wrap_contents/wrap_contents.sh
+++ wrap_contents/wrap_contents.sh
@@ -2,9 +2,10 @@
 
 #The "contents" that need to be wrapped by html header/footer
 contents=$1
+
 #User's desired header and footer
 headerFooterName=$2
 #The user chosen name of the resulting file
-outputFileName=$3
+outputFileName=$(basename -s . "$3")
 
-cat "$headerFooterName\_header" "$contents" "$headerFooterName_footer" > "$outputFile"
+cat "${headerFooterName}_header.html" "$contents" "${headerFooterName}_footer.html" > "$outputFile"
