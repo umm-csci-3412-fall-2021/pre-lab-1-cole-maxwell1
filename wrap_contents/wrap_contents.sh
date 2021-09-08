bf2f1d95dee5a843bbf8b10bd30284a50b69c66a
cole-maxwell1 <cole.n.maxwell99@gmail.com>
2021-09-08 17:49:53 -0500

    Fix outputFileName

--- wrap_contents/wrap_contents.sh
+++ wrap_contents/wrap_contents.sh
@@ -2,10 +2,10 @@
 
 #The "contents" that need to be wrapped by html header/footer
 contents=$1
-
 #User's desired header and footer
 headerFooterName=$2
 #The user chosen name of the resulting file
-outputFileName=$(basename -s . "$3")
+outputFileName=$3
 
-cat "${headerFooterName}_header.html" "$contents" "${headerFooterName}_footer.html" > "$outputFile"
+#Concatinate files together into the output file
+cat "${headerFooterName}_header.html" "$contents" "${headerFooterName}_footer.html" > "$outputFileName"
